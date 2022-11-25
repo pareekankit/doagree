0f56ce4c659226b358833bd5d61714b7cd5af4fc
kuldeep <yadav01kam@gmail.com>
2022-11-25 16:01:50 +0530

    OTP Verification

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { VerifyOtpComponent } from './authentication/verify-otp/verify-otp.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'verify-otp',component:VerifyOtpComponent},
+ {path:'',redirectTo:'/verify-otp',pathMatch:'full'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
