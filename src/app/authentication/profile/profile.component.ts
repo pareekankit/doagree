9e8704c9294364bf12265c5d221c92c672682d1a
MUKESH-K-SARDIWAL <mukeshsardiwal1997@gmail.com>
2022-12-06 14:41:34 +0530

    06/12/2022-update code

--- src/app/authentication/profile/profile.component.ts
+++ src/app/authentication/profile/profile.component.ts
@@ -18,78 +18,91 @@ export class ProfileComponent implements OnInit {
   @ViewChild('Gender') Gender!: ElementRef;
 
   
-  constructor(private apiCallMethod:ApiCallMethodsService,private sanitizer: DomSanitizer,private Route:ActivatedRoute,private router:Router) {
-    apiCallMethod.get(apiRoutes.getProfile).then((response:any)=>{
-      console.log(response.data)
-      this.RegisterForm.setValue(
+  constructor(private apiCallMethod:ApiCallMethodsService,private sanitizer: DomSanitizer,private Route:ActivatedRoute,private router:Router) 
+  {
+      apiCallMethod.get(apiRoutes.getProfile).then((response:any)=>{
+      
+        this.RegisterForm.setValue(
         {
-         validUserName:response.data.name,
-         validAge:response.data.age,
-         validReferalCode:response.data.reference_code,
-         validGender:response.data.gender,
-         validOccupation:response.data.occupation,
-         vaildMobileNo:response.data.phone,
-        },  
-      )
+                validUserName:response.data.name,
+                validAge:response.data.age,
+                validReferalCode:response.data.reference_code,
+                validGender:response.data.gender,
+                validOccupation:response.data.occupation,
+                vaildMobileNo:response.data.phone,
+        }); 
 
-    })
+      })
    
   }
+
+  ngOnInit(): void {
+  }
+  
   RegisterForm=new FormGroup({
-    validUserName: new FormControl('',[Validators.required]),
-    validAge: new FormControl('',[Validators.required]),
-    validReferalCode: new FormControl('',[Validators.required]),
-    validGender:new FormControl('',[Validators.required]),
-    validOccupation: new FormControl('',[Validators.required]),
-    vaildMobileNo:new FormControl(''),
+
+                validUserName: new FormControl('',[Validators.required]),
+                validAge: new FormControl('',[Validators.required]),
+                validReferalCode: new FormControl('',[Validators.required]),
+                validGender:new FormControl('',[Validators.required]),
+                validOccupation: new FormControl('',[Validators.required]),
+                vaildMobileNo:new FormControl(''),
   });
   
  
 
-	checkGender(SelectImgAvatar:any):void 
-      {
-        if(this.imgFlag==0)
-        {
-          if(this.Gender.nativeElement.value=='Male'){
-            SelectImgAvatar.src='/assets/Ellipse 94.png';
+	checkGender(SelectImgAvatar:any):void{
+          if(this.imgFlag==0)
+          {
+
+              if(this.Gender.nativeElement.value=='Male'){
+                SelectImgAvatar.src='/assets/Ellipse 94.png';
+              }
+
+              else{
+                SelectImgAvatar.src='/assets/Femalee-farmer 2.png';
+              }
           }
-          else{
-            SelectImgAvatar.src='/assets/Femalee-farmer 2.png';
-          }
-        }
-      }
+  }
   
   goOnLoginPage(){
-        this.router.navigate(['/login']);
-      }
-  saveAndContinue(event:any)
-      {
-          event.target.disable='true';
-          let data : any = {
-            name:this.RegisterForm.value.validUserName,
-            picture:this.AvatarImg,
-            gender:this.RegisterForm.value.validGender,
-            age:this.RegisterForm.value.validAge,
-            reference_code:this.RegisterForm.value.validReferalCode,
-            mobile_no:this.RegisterForm.value.vaildMobileNo,
-            occupation:this.RegisterForm.value.validOccupation
-          } 
-          this.apiCallMethod.post(apiRoutes.profile,data).
-          then((response:any)=>{
+
+      this.router.navigate(['/login']);
+
+  }
+
+  saveAndContinue(event:any){
+
+      event.target.disable='true';
+      let data : any = {
+        name:this.RegisterForm.value.validUserName,
+        picture:this.AvatarImg,
+        gender:this.RegisterForm.value.validGender,
+        age:this.RegisterForm.value.validAge,
+        reference_code:this.RegisterForm.value.validReferalCode,
+        mobile_no:this.RegisterForm.value.vaildMobileNo,
+        occupation:this.RegisterForm.value.validOccupation
+      } 
+      this.apiCallMethod.post(apiRoutes.profile,data).
+         then((response:any)=>{
             console.log(response);
             this.router.navigate(['/geolocation']);
-          }).catch((error:any)=>{
+         }).catch((error:any)=>{
             console.log(error);
-          })
-      }
-      openGellary(fileInput:HTMLInputElement)
-      {
+         })
+  }
+
+  openGellary(fileInput:HTMLInputElement){
+
         fileInput.click();
         console.log(fileInput)
-      }
-      onSelectFile(event:any) {
-          if (event.target.files && event.target.files[0]) 
-          {
+      
+  }
+     
+  onSelectFile(event:any){
+
+        if (event.target.files && event.target.files[0]) 
+        {
 
                 var reader = new FileReader();
 
@@ -100,11 +113,10 @@ export class ProfileComponent implements OnInit {
                   this.url = event.target.result;
                   this.imgFlag=1;
                 }
-          }
-      }
-  
-  
-  ngOnInit(): void {
+        }
   }
+  
+  
+  
 
 }
