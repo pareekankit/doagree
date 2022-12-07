4d04ae3dd13b0fc9f5c542964bb2dd358b8a0011
MUKESH-K-SARDIWAL <mukeshsardiwal1997@gmail.com>
2022-12-07 18:06:02 +0530

    card code commit

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,12 +1,23 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { CardComponent } from './component/card/card.component';
+import { SellerCardComponent } from './component/seller-card/seller-card.component';
+
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    CardComponent,
+    SellerCardComponent
+  ],
   imports: [
     CommonModule
+  ],
+  exports:[
+    CardComponent,
+    SellerCardComponent
   ]
+
 })
 export class SharedModule { }
